a5489ef8325b709bb3e3ab5a0ce793dfe01ac6c6
Okan YILDIRIM <63511519+ragokan@users.noreply.github.com>
2022-02-25 23:22:37 +0300

    types

--- packages/nuxt/src/runtime/plugin.ts
+++ packages/nuxt/src/runtime/plugin.ts
@@ -2,6 +2,18 @@
 import { defineNuxtPlugin } from "#app";
 import { KogaraInstance, KogaraPlugin } from "@kogara/core";
 
+declare module "vue" {
+  interface ComponentCustomProperties {
+    $kogara: typeof KogaraInstance;
+  }
+}
+
+declare module "@vue/runtime-core" {
+  interface ComponentCustomProperties {
+    $kogara: typeof KogaraInstance;
+  }
+}
+
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(KogaraPlugin);
 

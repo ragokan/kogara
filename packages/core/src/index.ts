80f9f65713ab3c9981bdd742c43409dee35991a5
Okan YILDIRIM <63511519+ragokan@users.noreply.github.com>
2022-02-27 20:36:37 +0300

    rename file

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,5 @@
 /// <reference path="../../../global.d.ts" />
 
-export { defineStore } from "./lib/kogara";
+export { defineStore } from "./lib/store";
 export { KogaraInstance } from "./lib/instance";
 export { KogaraPlugin } from "./lib/vue";

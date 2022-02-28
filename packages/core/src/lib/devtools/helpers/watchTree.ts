7eb280cf69c36f33ab3bdc0bc1bf5a1ed89bdcbc
Okan YILDIRIM <63511519+ragokan@users.noreply.github.com>
2022-02-28 21:40:52 +0300

    tree

--- packages/core/src/lib/devtools/helpers/watchTree.ts
+++ packages/core/src/lib/devtools/helpers/watchTree.ts
@@ -4,8 +4,15 @@ import { KogaraInstance } from "../../instance";
 import { kogaraDevtoolsID } from "../constants";
 
 export const _watchTree = (api: DevtoolsPluginApi<any>) => {
-  watch(KogaraInstance.stores, () => {
-    api.sendInspectorTree(kogaraDevtoolsID);
-    api.sendInspectorState(kogaraDevtoolsID);
-  });
+  watch(
+    KogaraInstance.stores,
+    (current, previous) => {
+      if (Object.keys(previous ?? {}).length !== Object.keys(current).length) {
+        console.log("not equal");
+        api.sendInspectorTree(kogaraDevtoolsID);
+      }
+      api.sendInspectorState(kogaraDevtoolsID);
+    },
+    { deep: true, immediate: true }
+  );
 };

6dc5b5a988187ac043ab49d671033132d64ec946
Okan YILDIRIM <63511519+ragokan@users.noreply.github.com>
2022-03-06 21:24:31 +0300

    debounce

--- packages/helpers/src/index.ts
+++ packages/helpers/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./lib/sleep";
 export * from "./lib/types";
 export * from "./lib/array";
+export * from "./lib/debounce";
---
+++ packages/helpers/src/lib/debounce.ts
@@ -0,0 +1,10 @@
+type ArgsType<T> = T extends (...args: infer U) => any ? U : never;
+
+export const debounce = <T extends Function>(func: T, duration = 300) => {
+  let timeout: NodeJS.Timeout;
+
+  return (...args: ArgsType<T>) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(this, args), duration);
+  };
+};

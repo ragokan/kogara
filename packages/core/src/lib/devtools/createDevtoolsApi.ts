1f4a93a7f65d033e552067b823e81f33e156e82c
Okan YILDIRIM <63511519+ragokan@users.noreply.github.com>
2022-05-04 23:37:29 +0300

    fix async errors

--- packages/core/src/lib/devtools/createDevtoolsApi.ts
+++ packages/core/src/lib/devtools/createDevtoolsApi.ts
@@ -21,13 +21,14 @@ export const _createDevtoolsApi = (id: string, data: any): KogaraDevtoolsApi[] |
       // If the type is a function, we change it to have logging function
       if (type === "other" && isFunction(value)) {
         const func = Object.assign({}, data)[key] as Function;
+
         Object.defineProperty(data, key, {
-          async value(...args: any[]) {
+          value(...args: any[]) {
             let functionValue: any;
             _addLog({ id, args, data, key, message: "started" });
             try {
               // If the function returns a promise, we wait for it to resolve
-              functionValue = await func(...args);
+              functionValue = func(args);
             } catch (error) {
               // We catch the error to add it to the timeline but we also throw it again, so that the behaviour does not change in the release
               _addLog({ id, args, data, key, message: "failed", logType: "error", error: String(error ?? "") });

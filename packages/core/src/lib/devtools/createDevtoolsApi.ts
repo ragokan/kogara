cb26f9ddc0c90cf121da194fd9dcf5ec216177df
Okan YILDIRIM <63511519+ragokan@users.noreply.github.com>
2022-06-20 22:12:41 +0300

    func logging

--- packages/core/src/lib/devtools/createDevtoolsApi.ts
+++ packages/core/src/lib/devtools/createDevtoolsApi.ts
@@ -20,25 +20,46 @@ export const _createDevtoolsApi = (id: string, data: any): KogaraDevtoolsApi[] |
 
       // If the type is a function, we change it to have logging function
       if (type === "other" && isFunction(value)) {
-        // Do nothing for now
-        // const func = Object.assign({}, data)[key] as Function;
-        // Object.defineProperty(data, key, {
-        //   value(...args: any[]) {
-        //     let functionValue: any;
-        //     _addLog({ id, args, data, key, message: "started" });
-        //     try {
-        //       // If the function returns a promise, we wait for it to resolve
-        //       functionValue = func(args);
-        //     } catch (error) {
-        //       // We catch the error to add it to the timeline but we also throw it again, so that the behaviour does not change in the release
-        //       _addLog({ id, args, data, key, message: "failed", logType: "error", error: String(error ?? "") });
-        //       throw error;
-        //     } finally {
-        //       _addLog({ id, args, data, key, message: "ended" });
-        //     }
-        //     return functionValue;
-        //   },
-        // });
+        const func = data[key] as Function;
+        const isAsyncFunction = func.constructor.name === "AsyncFunction";
+
+        if (isAsyncFunction) {
+          return Object.defineProperty(data, key, {
+            async value(...args: any[]) {
+              let functionResult: any;
+              _addLog({ id, args, data, key, message: "started" });
+
+              try {
+                functionResult = await func(args);
+              } catch (error) {
+                // We catch the error to add it to the timeline but we also throw it again, so that the behaviour does not change in the release.
+                _addLog({ id, args, data, key, message: "failed", logType: "error", error: String(error ?? "") });
+                throw error;
+              }
+
+              _addLog({ id, args, data, key, message: "ended" });
+              return functionResult;
+            },
+          });
+        }
+
+        // It is not async function, so we just add the logging function.
+        Object.defineProperty(data, key, {
+          value(...args: any[]) {
+            let functionResult: any;
+            _addLog({ id, args, data, key, message: "started" });
+
+            try {
+              functionResult = func(args);
+            } catch (error) {
+              _addLog({ id, args, data, key, message: "failed", logType: "error", error: String(error ?? "") });
+              throw error;
+            }
+
+            _addLog({ id, args, data, key, message: "ended" });
+            return functionResult;
+          },
+        });
       }
     });
     return devtoolsApi;

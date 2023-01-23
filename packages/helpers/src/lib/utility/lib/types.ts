98283c9834f4abfdca315ccf4b8740675bd36fe5
ragokan <okanyldrmop@gmail.com>
2023-01-23 11:40:59 +0300

    make checkings faster

--- packages/helpers/src/lib/utility/lib/types.ts
+++ packages/helpers/src/lib/utility/lib/types.ts
@@ -1,5 +1,5 @@
-export const isArray = (value: any): value is any[] => value && Array.isArray(value);
+export const isArray = (value: any): value is any[] => value && value instanceof Array;
 
-export const isObject = (value: any): value is object => value && typeof value === "object" && !Array.isArray(value);
+export const isObject = (value: any): value is object => value && typeof value === "object" && !(value instanceof Array);
 
 export const isFunction = (value: any): value is Function => value && typeof value === "function";

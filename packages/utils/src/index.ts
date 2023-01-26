35f24b0021000843b8d9b724897ef48108f3dc2f
ragokan <okanyldrmop@gmail.com>
2023-01-26 21:07:49 +0300

    rename folder

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,5 +1,5 @@
 export * from "./lib/clone";
-export * from "./lib/extend/array";
+export * from "./lib/extend-types/array";
 export * from "./lib/debounce";
 export * from "./lib/sleep";
 export * from "./lib/types";

09b59c951c15df63068d150a9e806fe11067e02b
ragokan <okanyldrmop@gmail.com>
2023-01-27 21:38:58 +0300

    add get random element

--- packages/utils/src/lib/extend-types/array.ts
+++ packages/utils/src/lib/extend-types/array.ts
@@ -1,2 +1,5 @@
 export const insert = <T>(array: T[], index: number, ...items: T[]) =>
   array.splice(index, 0, ...items);
+
+export const getRandomElement = <T>(arr: Array<T>): T | undefined =>
+  arr[Math.floor(Math.random() * arr.length)];

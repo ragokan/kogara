d30332a987b1e326cf55940b02c84b48177c1a07
ragokan <okanyldrmop@gmail.com>
2023-01-31 22:43:21 +0300

    use guard clause

--- packages/core/src/lib/devtools/helpers/inspectorState.ts
+++ packages/core/src/lib/devtools/helpers/inspectorState.ts
@@ -11,41 +11,42 @@ export const _getInspectorState = (api: DevtoolsPluginApi<any>, app: App<any>) =
 
     const { nodeId } = payload;
     const store = KogaraInstance.stores[nodeId];
-
-    if (store) {
-      const refOrReactives =
-        store.devtoolsApi?.filter((i) => i.type === "ref" || i.type === "reactive") ?? [];
-      const computeds = store.devtoolsApi?.filter((i) => i.type === "computed") ?? [];
-      const functions = store.devtoolsApi?.filter((i) => typeof i.value === "function") ?? [];
-      const state: CustomInspectorState = {};
-
-      // Add reactive values if they exist
-      if (refOrReactives.length) {
-        state.state = refOrReactives.map((data) => {
-          const isRef = data.type === "ref";
-          return {
-            key: data.key,
-            value: isRef ? data.value.value : data.value,
-            objectType: data.type,
-            editable: isRef || data.type === "reactive",
-          };
-        });
-      }
-      // Add computed variables if they exist
-      if (computeds.length) {
-        state.computed = computeds.map((data) => ({
-          key: data.key,
-          value: data.value.value,
-          objectType: data.type,
-        }));
-      }
-
-      // Add functions if they exist
-      if (functions.length) {
-        state.functions = functions.map((data) => ({ key: data.key, value: data.value }));
-      }
-
-      payload.state = state;
+    if (!store) {
+      return;
     }
+
+    const refOrReactives =
+      store.devtoolsApi?.filter((i) => i.type === "ref" || i.type === "reactive") ?? [];
+    const computeds = store.devtoolsApi?.filter((i) => i.type === "computed") ?? [];
+    const functions = store.devtoolsApi?.filter((i) => typeof i.value === "function") ?? [];
+    const state: CustomInspectorState = {};
+
+    // Add reactive values if they exist
+    if (refOrReactives.length) {
+      state.state = refOrReactives.map((data) => {
+        const isRef = data.type === "ref";
+        return {
+          key: data.key,
+          value: isRef ? data.value.value : data.value,
+          objectType: data.type,
+          editable: isRef || data.type === "reactive",
+        };
+      });
+    }
+    // Add computed variables if they exist
+    if (computeds.length) {
+      state.computed = computeds.map((data) => ({
+        key: data.key,
+        value: data.value.value,
+        objectType: data.type,
+      }));
+    }
+
+    // Add functions if they exist
+    if (functions.length) {
+      state.functions = functions.map((data) => ({ key: data.key, value: data.value }));
+    }
+
+    payload.state = state;
   });
 };

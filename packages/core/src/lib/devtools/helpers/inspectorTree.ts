b8ad648d41a462ef71cabfae55d2dc2abb81c7b9
ragokan <okanyldrmop@gmail.com>
2023-01-31 21:01:07 +0300

    add tag to store

--- packages/core/src/lib/devtools/helpers/inspectorTree.ts
+++ packages/core/src/lib/devtools/helpers/inspectorTree.ts
@@ -1,4 +1,4 @@
-import type { CustomInspectorNode, DevtoolsPluginApi } from "@vue/devtools-api";
+import type { CustomInspectorNode, DevtoolsPluginApi, InspectorNodeTag } from "@vue/devtools-api";
 import type { App } from "vue";
 import { KogaraInstance } from "../../instance";
 import { kogaraDevtoolsID } from "../constants";
@@ -18,6 +18,7 @@ export const _getInspectorTree = (api: DevtoolsPluginApi<any>, app: App<any>) =>
         id: "stores",
         label: "Kogara Stores",
         children: stores.map(({ id }) => ({ id, label: id })),
+        tags: [getTag(stores.length)],
       });
     } else {
       nodes.push({
@@ -29,3 +30,10 @@ export const _getInspectorTree = (api: DevtoolsPluginApi<any>, app: App<any>) =>
     payload.rootNodes = nodes;
   });
 };
+function getTag(label: number | string): InspectorNodeTag {
+  return {
+    label: label.toString(),
+    backgroundColor: 0x3ba776,
+    textColor: 0x000000,
+  };
+}

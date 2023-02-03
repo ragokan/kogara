d4fb212a597ed51b3a3da95219f01921b41a4692
ragokan <okanyldrmop@gmail.com>
2023-02-03 23:35:58 +0300

    mini ref update

--- packages/helpers/src/lib/reactivity/lib/miniRef/index.ts
+++ packages/helpers/src/lib/reactivity/lib/miniRef/index.ts
@@ -1,12 +1,12 @@
 import { customRef } from "vue";
 
 export function miniRef<T>(value: T) {
-  return customRef((track, trigger) => ({
+  return customRef<T>((track, trigger) => ({
     get() {
       track();
       return value;
     },
-    set(newValue: T) {
+    set(newValue) {
       if (newValue === value) {
         return;
       }

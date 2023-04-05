cc5a1ff707b199cdb3c6504e495adda4e724c079
ragokan <okanyldrmop@gmail.com>
2023-04-05 14:35:09 +0300

    vanillaa

--- packages/preact-signals/src/lib/vanilla.ts
+++ packages/preact-signals/src/lib/vanilla.ts
@@ -1,3 +1,65 @@
-export function y() {
-  return 0;
+import { clone, isArray, isFunction } from "@kogara/utils";
+import {
+  signal as baseSignal,
+  computed as baseComputed,
+  effect as baseEffect,
+  batch as baseBatch,
+} from "@preact/signals";
+import { Computed, Signal } from "./vanilla.type";
+
+export function signal<T>(value: T): Signal<T> {
+  const base = baseSignal(value);
+
+  function fn() {
+    return base.value;
+  }
+
+  function set(value: T) {
+    if (value === base.peek()) {
+      return;
+    }
+    base.value = value;
+  }
+
+  fn.get = base.peek();
+
+  fn.set = set;
+
+  fn.update = (fn: (value: T) => T) => set(fn(base.peek()));
+
+  if (typeof value === "object") {
+    fn.mutate = (fn: (value: T) => void) => {
+      const copy = clone(base.peek() as object);
+      fn(copy as T);
+      set(copy as T);
+    };
+
+    if (!isArray(value)) {
+      fn.partial = (maybeFn: ((value: T) => Partial<T>) | Partial<T>) => {
+        const current = base.peek();
+        const part = isFunction(maybeFn) ? maybeFn(current) : maybeFn;
+        set(Object.assign({}, current, part));
+      };
+    }
+  }
+
+  fn.subscribe = base.subscribe;
+
+  return fn as Signal<T>;
 }
+
+export function computed<T>(compute: () => T): Computed<T> {
+  const base = baseComputed(compute);
+
+  function fn() {
+    return base.value;
+  }
+
+  fn.get = base.peek();
+
+  fn.subscribe = base.subscribe;
+
+  return fn;
+}
+
+export { baseEffect as effect, baseBatch as batch };
---
+++ packages/preact-signals/src/lib/vanilla.type.ts
@@ -0,0 +1,27 @@
+interface BaseSignal<T> {
+  (): T;
+  get: T;
+  set(value: T): void;
+  update(fn: (value: T) => T): void;
+  subscribe: (fn: (value: T) => void) => () => void;
+}
+
+interface ObjectSignal<T extends object> extends BaseSignal<T> {
+  mutate(fn: (value: T) => void): void;
+}
+
+interface RecordSignal<T extends Record<any, any>> extends ObjectSignal<T> {
+  partial(maybeFn: ((value: T) => Partial<T>) | Partial<T>): void;
+}
+
+export type Signal<T> = T extends object
+  ? T extends Array<any>
+    ? ObjectSignal<T>
+    : RecordSignal<T>
+  : BaseSignal<T>;
+
+export interface Computed<T> {
+  (): T;
+  get: T;
+  subscribe: (fn: (value: T) => void) => () => void;
+}

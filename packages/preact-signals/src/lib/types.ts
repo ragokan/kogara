c40cfafee6d4c74fc70b1bba7d2625b9e4fd5125
ragokan <okanyldrmop@gmail.com>
2023-06-11 21:46:18 +0300

    fisu

--- packages/preact-signals/src/lib/types.ts
+++ packages/preact-signals/src/lib/types.ts
@@ -18,7 +18,8 @@ interface RecordSignal<T extends object> extends ObjectSignal<T> {
   partial(maybeFn: ((value: T) => Partial<T>) | Partial<T>): void;
 }
 
-interface MaybeRecordSignal<T extends object | null> {
+interface MaybeRecordSignal<T extends object | null>
+  extends BaseSignal<UnwrapMaybe<T>> {
   maybeMutate(
     fn: (value: NonNullable<T>) => void,
     otherwise?: (tryAgain: () => void) => void
@@ -27,23 +28,17 @@ interface MaybeRecordSignal<T extends object | null> {
     maybeFn: ((value: NonNullable<T>) => Partial<T>) | Partial<T>,
     otherwise?: (tryAgain: () => void) => void
   ): void;
-  set(value: UnwrapMaybe<T>): void;
-  update: (fn: (value: T) => UnwrapMaybe<T>) => void;
-  (): UnwrapMaybe<T>;
-  get(): UnwrapMaybe<T>;
 }
 
 type RecordOrArraySignal<T extends object> = T extends Array<any>
   ? ObjectSignal<T>
   : RecordSignal<T>;
 
-type MergeSignal<T> = T extends Maybe<object>
+export type Signal<T> = T extends Maybe<object>
   ? MaybeRecordSignal<T>
   : T extends object
   ? RecordOrArraySignal<T>
   : {};
-
-export type Signal<T> = BaseSignal<T> & MergeSignal<T>;
 
 export interface Computed<T> {
   (): T;

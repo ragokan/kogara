08384c14759362f211bdf1e9609b6a4638d5a8f8
ragokan <okanyldrmop@gmail.com>
2023-09-10 22:54:56 +0300

    add nuxt back

--- packages/nuxt/playground/app.vue
+++ packages/nuxt/playground/app.vue
@@ -3,12 +3,13 @@
     <p>Nuxt module playground!</p>
     <p>Count: {{ count }}</p>
     <p>Doubled Count: {{ doubledCount }}</p>
-    <button @click="count++">Click me</button>
+    <button @click="count++">
+      Click me
+    </button>
   </div>
 </template>
 
 <script setup lang="ts">
-  import { KogaraInstance } from "@kogara/core";
   import { useCounterStore } from "./stores/counterStore";
   const { count, doubledCount } = useCounterStore();
   if (process.server) {
